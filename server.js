8c59f9d0adac093d3da7501ed75bd09b245babb4
Jake Williams <jakemwilliams2@gmail.com>
2017-04-04 19:15:30 -0400

    Postgres

--- server.js
+++ server.js
@@ -39,9 +39,8 @@ app.listen(port, function () {
 console.log(process.env.DATABASE_URL);
 // pg.connect('http://ec2-23-21-220-23.compute-1.amazonaws.com', function(err, client, done) {
 pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-  // client.query("CREATE TABLE test(id SERIAL PRIMARY KEY)", function(err, result) {
-    // done();
-    // if(err) return console.error(err);
-  // }); 
-  console.log("EY");
+  client.query("CREATE TABLE test(id SERIAL PRIMARY KEY)", function(err, result) {
+    done();
+    if(err) return console.error(err);
+  }); 
 });
