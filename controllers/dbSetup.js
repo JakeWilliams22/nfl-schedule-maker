b3e976f1d3547384f558bb366a676b4e48683d7f
Jake Williams <jakemwilliams2@gmail.com>
2017-04-04 20:57:37 -0400

    Postgres

--- controllers/dbSetup.js
+++ controllers/dbSetup.js
@@ -7,8 +7,8 @@ function createPreferencesTable(req, res, next) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query("CREATE TABLE IF NOT EXISTS preferences(id SERIAL PRIMARY KEY, name TEXT NOT NULL, priority INT, team TEXT, scheduleYear INT)", function(err, result) {
       done();
-      if(err) return console.error(err);
-      console.log("Success");
+      if(err) return res.send(err);
+      res.send("Creation Success");
     }); 
   });
 }
@@ -17,7 +17,8 @@ function testPreferenceInsert(req, res, next) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query("INSERT INTO preferences VALUES (2, 'test', 1, 'panthers', 2017);", function(err, result) {
       done();
-      if(err) return console.error(err);
+      if(err) return res.send(err);
+      res.send("Insert Success");
     });
   });
 }
