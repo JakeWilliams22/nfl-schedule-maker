51ed14977dd4130e3348ac4e6f13515a70f58418
Jake Williams <jakemwilliams2@gmail.com>
2017-04-04 20:13:09 -0400

    Postgres

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,18 @@
 'use strict';
 
 require('rootpath')();
+var pg = require('pg');
 
 module.exports = function(app, router) {
   router.get('/hello/', function(request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+      // client.query("CREATE TABLE test(id SERIAL PRIMARY KEY)", function(err, result) {
+      client.query("SELECT * FROM TABLE test", function(err, result) {
+        done();
+        if(err) return console.error(err);
+        console.log(result.rows);
+      }); 
+    });
     response.send('Hi World!')
   });
   
