5608edd8e83299d1de27c8bc8a17d2cca9f09ae8
Jake Williams <jakemwilliams2@gmail.com>
2017-04-11 08:48:23 -0400

    Random Test

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -3,12 +3,12 @@
 require('rootpath')();
 var sched_dict = require('res/schedule_dict');
 var pg = require('pg');
-nfl_teams = sched_dict.nfl_teams;
+var nfl_teams = sched_dict.nfl_teams;
 
 games = [];
 
 function getRandomSchedule(req, res, next) {
-
+//  res.send(sched_dict.nfl_teams[1].city);
   var teamIndex = 0;
   for (var game = 0; game < 256; game++) {
     team = nfl_teams[teamIndex];
