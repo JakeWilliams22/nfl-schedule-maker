ac3ac24eeab457cef19974d0cde2a4ab31c81b3a
Jake Williams <jakemwilliams2@gmail.com>
2017-04-11 08:49:19 -0400

    Random Test

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -5,14 +5,14 @@ var sched_dict = require('res/schedule_dict');
 var pg = require('pg');
 var nfl_teams = sched_dict.nfl_teams;
 
-games = [];
+var games = [];
 
 function getRandomSchedule(req, res, next) {
 //  res.send(sched_dict.nfl_teams[1].city);
   var teamIndex = 0;
   for (var game = 0; game < 256; game++) {
-    team = nfl_teams[teamIndex];
-    opponentNum = Math.floor((Math.random() * (31-teamIndex)) + teamIndex + 1);
+    var team = nfl_teams[teamIndex];
+    var opponentNum = Math.floor((Math.random() * (31-teamIndex)) + teamIndex + 1);
     games.push([nfl_teams[teamIndex],nfl_teams[opponentNum]]);
     
     if(team.numGames == 16) {
