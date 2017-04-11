34b3dd0eb3b057c9b2c3976cea8f157807b58b48
Jake Williams <jakemwilliams2@gmail.com>
2017-04-11 08:52:43 -0400

    Random Test

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -12,7 +12,11 @@ function getRandomSchedule(req, res, next) {
   var teamIndex = 0;
   for (var game = 0; game < 256; game++) {
     var team = nfl_teams[teamIndex];
-    var opponent = nfl_teams[Math.floor((Math.random() * (31-teamIndex)) + teamIndex + 1)];
+    var opponent;
+    do {
+      opponent = nfl_teams[Math.floor((Math.random() * (31-teamIndex)) + teamIndex + 1)];
+    } while (opponent.numGames >= 16);
+    
     games.push([team,opponent]);
     
     team.numGames = team.numGames + 1;
