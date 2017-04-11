6af8442e2fae6041b3f0d8681f58be56202a51cf
Jake Williams <jakemwilliams2@gmail.com>
2017-04-11 08:47:18 -0400

    Random Test

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -3,9 +3,24 @@
 require('rootpath')();
 var sched_dict = require('res/schedule_dict');
 var pg = require('pg');
+nfl_teams = sched_dict.nfl_teams;
+
+games = [];
 
 function getRandomSchedule(req, res, next) {
-  res.send(sched_dict.nfl_teams[1].city);
+
+  var teamIndex = 0;
+  for (var game = 0; game < 256; game++) {
+    team = nfl_teams[teamIndex];
+    opponentNum = Math.floor((Math.random() * (31-teamIndex)) + teamIndex + 1);
+    games.push([nfl_teams[teamIndex],nfl_teams[opponentNum]]);
+    
+    if(team.numGames == 16) {
+      teamIndex++;
+    }
+  }
+  
+  res.send(games);
 }
 
 exports.getRandomSchedule = getRandomSchedule;
--- res/schedule_dict.js
+++ res/schedule_dict.js
@@ -3,43 +3,44 @@
 require('rootpath')();
 
 var nfl_teams = {
-  1: new NFL_TEAM('Arizona', 'Cardinals'),
-  2: new NFL_TEAM('Atlanta', 'Falcons'),
-  3: new NFL_TEAM('Baltimore', 'Ravens'),
-  4: new NFL_TEAM('Bufalo', 'Bills'),
-  5: new NFL_TEAM('Carolina', 'Panthers'),
-  6: new NFL_TEAM('Chicago', 'Bears'),
-  7: new NFL_TEAM('Cincinnati', 'Bengals'),
-  8: new NFL_TEAM('Cleveland', 'Browns'),
-  9: new NFL_TEAM('Dallas', 'Cowboys'),
-  10: new NFL_TEAM('Denver', 'Broncos'),
-  11: new NFL_TEAM('Detroit', 'Lions'),
-  12: new NFL_TEAM('Green Bay', 'Packers'),
-  13: new NFL_TEAM('Houston', 'Texans'),
-  14: new NFL_TEAM('Indianapolis', 'Colts'),
-  15: new NFL_TEAM('Jacksonville', 'Jaguars'),
-  16: new NFL_TEAM('Kansas City', 'Chiefs'),
-  17: new NFL_TEAM('Miami', 'Dolphins'),
-  18: new NFL_TEAM('Minnesota', 'Vikings'),
-  19: new NFL_TEAM('New England', 'Patriots'),
-  20: new NFL_TEAM('New Orleans', 'Saints'),
-  21: new NFL_TEAM('New York', 'Giants'),
-  22: new NFL_TEAM('New York', 'Jets'),
-  23: new NFL_TEAM('Oakland', 'Raiders'),
-  24: new NFL_TEAM('Philadelpia', 'Eagles'),
-  25: new NFL_TEAM('Pittsburgh', 'Steelers'),
-  26: new NFL_TEAM('San Diego', 'Chargers'),
-  27: new NFL_TEAM('San Francisco', '49ers'),
-  28: new NFL_TEAM('Seattle', 'Seahawks'),
-  29: new NFL_TEAM('St. Louis', 'Rams'),
-  30: new NFL_TEAM('Tampa Bay', 'Buccaneers'),
-  31: new NFL_TEAM('Tennessee', 'Titans'),
-  32: new NFL_TEAM('Washington', 'Redskins')
+  0: new NFL_TEAM('Arizona', 'Cardinals'),
+  1: new NFL_TEAM('Atlanta', 'Falcons'),
+  2: new NFL_TEAM('Baltimore', 'Ravens'),
+  3: new NFL_TEAM('Bufalo', 'Bills'),
+  4: new NFL_TEAM('Carolina', 'Panthers'),
+  5: new NFL_TEAM('Chicago', 'Bears'),
+  6: new NFL_TEAM('Cincinnati', 'Bengals'),
+  7: new NFL_TEAM('Cleveland', 'Browns'),
+  8: new NFL_TEAM('Dallas', 'Cowboys'),
+  9: new NFL_TEAM('Denver', 'Broncos'),
+  10: new NFL_TEAM('Detroit', 'Lions'),
+  11: new NFL_TEAM('Green Bay', 'Packers'),
+  12: new NFL_TEAM('Houston', 'Texans'),
+  13: new NFL_TEAM('Indianapolis', 'Colts'),
+  14: new NFL_TEAM('Jacksonville', 'Jaguars'),
+  15: new NFL_TEAM('Kansas City', 'Chiefs'),
+  16: new NFL_TEAM('Miami', 'Dolphins'),
+  17: new NFL_TEAM('Minnesota', 'Vikings'),
+  18: new NFL_TEAM('New England', 'Patriots'),
+  19: new NFL_TEAM('New Orleans', 'Saints'),
+  20: new NFL_TEAM('New York', 'Giants'),
+  21: new NFL_TEAM('New York', 'Jets'),
+  22: new NFL_TEAM('Oakland', 'Raiders'),
+  23: new NFL_TEAM('Philadelpia', 'Eagles'),
+  24: new NFL_TEAM('Pittsburgh', 'Steelers'),
+  25: new NFL_TEAM('San Diego', 'Chargers'),
+  26: new NFL_TEAM('San Francisco', '49ers'),
+  27: new NFL_TEAM('Seattle', 'Seahawks'),
+  28: new NFL_TEAM('St. Louis', 'Rams'),
+  29: new NFL_TEAM('Tampa Bay', 'Buccaneers'),
+  30: new NFL_TEAM('Tennessee', 'Titans'),
+  31: new NFL_TEAM('Washington', 'Redskins')
 }
 
 function NFL_TEAM(city, mascot) {
   this.city = city;
   this.mascot = mascot;
+  this.numGames = 0;
 }
 
 exports.NFL_TEAM = NFL_TEAM;
