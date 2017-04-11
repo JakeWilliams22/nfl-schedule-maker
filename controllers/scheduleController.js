696fca08ad9631884dc1ab7196c88b742b4b8b29
Jake Williams <jakemwilliams2@gmail.com>
2017-04-11 08:37:12 -0400

    Random Fix

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -5,7 +5,7 @@ var sched_dict = require('res/schedule_dict');
 var pg = require('pg');
 
 function getRandomSchedule(req, res, next) {
-  res.send(sched_dict.nfl_teams[0].city);
+  res.send(sched_dict.nfl_teams[1].city);
 }
 
 exports.getRandomSchedule = getRandomSchedule;
