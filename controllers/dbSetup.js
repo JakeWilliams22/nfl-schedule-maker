b3223dd4ccae3179490c0f9c4d72e8ed61d6ebf4
jwilliams436 <jake.williams@gatech.edu>
2017-11-05 16:59:08 -0500

    Typo

--- controllers/dbSetup.js
+++ controllers/dbSetup.js
@@ -41,7 +41,7 @@ function testUserGet(req, res, next) {
 function deleteUser(req, res, next) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query("DELETE FROM users WHERE\
-                      username == " + req.username, function(err, result) {
+                      username = " + req.username, function(err, result) {
       done();
       if(err) return res.send(err);
       res.send("Creation Success");
